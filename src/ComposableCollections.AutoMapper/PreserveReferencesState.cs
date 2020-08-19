d89ca2a031119ef8e95178d9af5e33f84c347d07
nloum <39776302+nloum@users.noreply.github.com>
2020-08-19 09:31:49 -0500

    Add support for clearing the reference cache

--- src/ComposableCollections.AutoMapper/PreserveReferencesState.cs
+++ src/ComposableCollections.AutoMapper/PreserveReferencesState.cs
@@ -5,21 +5,38 @@ namespace ComposableCollections
 {
     public class PreserveReferencesState
     {
+        private readonly object _lock = new object();
         private IComposableDictionary<Type, object> _composableDictionaries = new ComposableDictionary<Type, object>();
+        private System.Collections.Generic.List<Action> _clearActions = new System.Collections.Generic.List<Action>();
         
         public IComposableDictionary<TKey, TValue> GetCache<TKey, TValue>()
         {
-            var key = typeof(IKeyValue<TKey, TValue>);
-            if (!_composableDictionaries.TryGetValue(key, out var result))
+            lock (_lock)
             {
-                var stronglyTypedResult = new ComposableDictionary<TKey, TValue>();
-                _composableDictionaries.Add(key, stronglyTypedResult);
-                return stronglyTypedResult;
+                var key = typeof(IKeyValue<TKey, TValue>);
+                if (!_composableDictionaries.TryGetValue(key, out var result))
+                {
+                    var stronglyTypedResult = new ComposableDictionary<TKey, TValue>();
+                    _composableDictionaries.Add(key, stronglyTypedResult);
+                    _clearActions.Add(() => stronglyTypedResult.Clear());
+                    return stronglyTypedResult;
+                }
+                else
+                {
+                    var stronglyTypedResult = (IComposableDictionary<TKey, TValue>) result;
+                    return stronglyTypedResult;
+                }
             }
-            else
+        }
+
+        public void Clear()
+        {
+            lock (_lock)
             {
-                var stronglyTypedResult = (IComposableDictionary<TKey, TValue>) result;
-                return stronglyTypedResult;
+                foreach (var clearAction in _clearActions)
+                {
+                    clearAction();
+                }
             }
         }
     }
